ec8413ad89ccc3cc0c31221e20c7d81e4f8e89e1
Jess Strzempko <70109499+jstrzempko@users.noreply.github.com>
2020-10-18 13:38:12 -0400

    Add files via upload

--- my_clip.py
+++ my_clip.py
@@ -1,5 +1,13 @@
-# Name: YOUR NAME HERE 
-# Python Version: ENTER ASSIGNMENT TITLE HERE.
-# Description: ENTER A DESCRIIPTION ABOUT WHAT YOUR CODE IS SUPPOSED TO DO HERE: DESCRIBE INPUTS / OUTPUTS.
+# Name: Jess Strzempko
+# Python Version: 2.7.15
+# Lab 1 Geoprocessing in ArcGIS
+# Description: This script clips the lakes layer to the basin boundaries.
+# It was created as Exercise 2 in Lab 1 Geoprocessing in ArcGIS for IDCE 30274 Computer Programming for GIS. 
+# Inputs: lakes.shp [layer of lakes], basin.shp [basin boundary]
+# Outputs: lakes_myClip.shp [final layer of lakes within basin boundary]
 
-Leave the lines of comment code, above, but add your personal details. Then replace these two lines of code with your new code! 
+import arcpy
+
+arcpy.env.workspace = "C:/Users/JeStrzempko/Documents/Comp_Prog/Lab1/Data_Lab_1_Geoprocessing_ArcGIS"
+
+arcpy.Clip_analysis("Data_Lab_1_Geoprocessing_ArcGIS/lakes.shp","Data_Lab_1_Geoprocessing_ArcGIS/basin.shp","Results/lakes_myClip.shp")
